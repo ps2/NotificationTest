3636c11b24d311d300c4b53fc04b4cc35e533178
Pete Schwamb <pete@schwamb.net>
2020-04-15 21:59:45 -0500

    Initial Commit

---
+++ NotificationTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NotificationTest
+//
+//  Created by Pete Schwamb on 4/15/20.
+//  Copyright © 2020 Tidepool. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
